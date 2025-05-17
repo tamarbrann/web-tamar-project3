73525e7e4e89ea4db808645a417d8f48620607c3
tamarbrann <brann@post.bgu.ac.il>
2025-05-17 15:19:17 +0300

    sounds

--- JS.js
+++ JS.js
@@ -1,1 +1,7 @@
-yolo
+//creating all buttons array
+var buttons=document.querySelectorAll(".animal");
+
+//adding event listener to the mouse click
+
+//adding event listener to the keyboard press
+
