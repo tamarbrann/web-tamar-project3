53bd6b425bd0139e0b8aa57c11ec0efedb565b4f
tamarbrann <brann@post.bgu.ac.il>
2025-05-17 17:05:07 +0300

    works

--- JS.js
+++ JS.js
@@ -1,69 +1,74 @@
-//creating all buttons array
-var buttons=document.querySelectorAll(".animal");
+const buttons = document.querySelectorAll(".animal");
 
+//adding event listener to the keyboard press
+document.addEventListener("keypress", function (event) {
+  makeSound(event.key);
+  buttonAnimation(event.key);
+});
 
-//Playing jungle music when the website loads
-window.addEventListener("load", function(){
-    var jungleAudio=new Audio("./sounds/african-rhythm.mp3");
+// window.addEventListener("click", function () {
+//   const jungleAudio = new Audio("sounds/junglesound.wav");
+//   jungleAudio.volume = 0.4;
+//   jungleAudio.play();
+// });
 
-//adding event listener to the mouse click
-for (var i=0;i<buttons.length;i++){
-    //void function that take the innerHTML of the button[i] and sends it to the makeSound function
-    buttons[i].addEventListener("click", function(){ 
-        var buttonInnerHTML=this.innerHTML;
-        makeSound(buttonInnerHTML);
-        buttonAnimation(buttonInnerHTML);
+for (const button of buttons) {
+  button.addEventListener("click", function () {
+    makeSound(button.innerText);
+  });
+}
+
+function buttonAnimation(currentKey) {
+  for (const button of buttons) {
+    if (button.innerText === currentKey) {
+      button.classList.add("pressed"); //will add a class of "pressed" to the pressed button
+      setTimeout(function () {
+        button.classList.remove("pressed");
+      }, 150); //after a delay of 100ms we'll remove the class
     }
-    )}
+  }
+}
 
-//adding event listener to the keyboard press
-document.addEventListener("keypress",function(event){
-    makeSound(event.key);
-    buttonAnimation(event.key);
-})
+let currentAudio;
 
-/*the function recieves a key as an input. based on the key (mouse click or keypress) 
-the relevant audio is being played*/
-var currentAudio; 
-
-
-//adding event listener to the keyboard press
 function makeSound(key) {
+  // Pause the currently playing audio
+  // Here I used two methods that were not taught in class -
+  // one is sound import and the other is speechSynthesis, which instead of importing sound,
+  // reads text that is written to it.
+  if (currentAudio) {
+    currentAudio.pause();
+  }
+  switch (key) {
+    case "c":
+      currentAudio = new Audio("sounds/catsound.wav");
+      break;
+    case "w":
+      currentAudio = new Audio("sounds/cowsound.wav");
+      break;
+    case "g":
+      speechSynthesis.speak(
+        new SpeechSynthesisUtterance("what a giraffe sounds like")
+      );
+      return;
+    case "h":
+      currentAudio = new Audio("sounds/horsesound.wav");
+      break;
+    case "l":
+      currentAudio = new Audio("sounds/Lion.mp3");
+      break;
+    case "d":
+      speechSynthesis.speak(
+        new SpeechSynthesisUtterance("I am Lucy and I am cute")
+      );
+      return;
+    case "s":
+      currentAudio = new Audio("sounds/sheepsound.wav");
+      break;
+    default:
+      alert("No key type!");
+      return;
+  }
 
-    // Pause the currently playing audio
-    // Here I used two methods that were not taught in class - 
-    // one is sound import and the other is speechSynthesis, which instead of importing sound, 
-    // reads text that is written to it.
-    if (currentAudio) {
-        currentAudio.pause();
-    }
-    switch (key) {
-        case 'c':
-            currentAudio = new Audio("sounds\catsound.wav");
-            break;
-        case 'w':
-            currentAudio = new Audio("sounds\cowsound.wav");
-            break;
-        case 'g':
-            speechSynthesis.speak(new SpeechSynthesisUtterance('what a giraffe sounds like'));
-            break;
-        case 'h':
-            currentAudio = new Audio("sounds\horsesound.wav");
-            break;
-        case 'l':
-            currentAudio = new Audio("sounds\Lion.mp3");
-            break;
-        case 'd':
-            speechSynthesis.speak(new SpeechSynthesisUtterance('I am Lucy and I am cute'))
-            break;
-        case 's':
-            currentAudio = new Audio("sounds\sheepsound.wav");
-            break;
-        default:
-            alert("No key type!");
-            return; // exit the function if the key is not recognized
-    }
-
-    // Play the new audio
-    currentAudio.play();
+  currentAudio.play();
 }
